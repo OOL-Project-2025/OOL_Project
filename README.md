9a2c7da06e6b213b7f10bddd1aeda7424ab10cd1
Pin <ppyj48625@naver.com>
2025-01-21 19:06:32 +0900

    init: 프로젝트 초기 설정

--- README.md
+++
@@ -1,1 +0,0 @@
-# OOL_Project
---
+++ src/main/java/com/OOL/oolfinance/OolFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.OOL.oolfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OolFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OolFinanceApplication.class, args);
+	}
+
+}
