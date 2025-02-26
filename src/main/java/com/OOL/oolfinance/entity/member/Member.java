940cde4a80ccf4ce06f703e6a8be77c0a4c2329d
Pin <ppyj48625@naver.com>
2025-02-26 23:27:17 +0900

    feat: member entity 추가

---
+++ src/main/java/com/OOL/oolfinance/entity/member/Member.java
@@ -0,0 +1,43 @@
+package com.OOL.oolfinance.entity.member;
+
+import com.OOL.oolfinance.entity.wishlist.Wishlist;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : Member
+ * @date : 2/17/25 / 10:00 PM
+ * @modifyed : $
+ **/
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 40, nullable = false)
+    private String memberId;
+
+    @Column(length = 40, nullable = false)
+    private String password;
+
+    @Column(length = 20, nullable = false)
+    private String nickname;
+
+    @Column
+    private String profilePhoto;
+
+    @OneToMany(mappedBy = "member", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Wishlist> category = new ArrayList<>();
+}
