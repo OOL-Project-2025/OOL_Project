aa08e98e03207fe5f4d9fdda75ddfb49ef3bfaed
Pin <ppyj48625@naver.com>
2025-03-27 00:13:00 +0900

    feat: 지수 종류 구분을 위한 Enum 추가 #11

---
+++ src/main/java/com/OOL/oolfinance/enums/IndexStatus.java
@@ -0,0 +1,16 @@
+package com.OOL.oolfinance.enums;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : IndexStatus
+ * @date : 3/25/25 / 9:51 PM
+ * @modifyed : $
+ **/
+public enum IndexStatus {
+    STOCK_INDEX,
+    BOND,
+    COMMODITY
+
+}
