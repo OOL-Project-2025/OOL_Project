aa73f44f0fb2f6b7016dd234c0985147481f0cdb
Pin <ppyj48625@naver.com>
2025-03-27 00:04:59 +0900

    feat: 주식 관련 Service 추가 #11

---
+++ src/main/java/com/OOL/oolfinance/service/stock/StockService.java
@@ -0,0 +1,12 @@
+package com.OOL.oolfinance.service.stock;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : StockService
+ * @date : 3/24/25 / 10:45 PM
+ * @modifyed : $
+ **/
+public interface StockService {
+}
---
+++ src/main/java/com/OOL/oolfinance/service/stock/StockServiceImpl.java
@@ -0,0 +1,20 @@
+package com.OOL.oolfinance.service.stock;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : StockServiceImpl
+ * @date : 3/24/25 / 10:45 PM
+ * @modifyed : $
+ **/
+
+@Service
+@RequiredArgsConstructor
+public class StockServiceImpl implements StockService{
+
+
+}
