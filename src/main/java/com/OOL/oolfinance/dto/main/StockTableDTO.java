2d71333f64b3bdc9a9043186c394de1395694d80
Pin <ppyj48625@naver.com>
2025-03-27 00:12:38 +0900

    feat: 메인 페이지 주식 테이블 요청에 대한 DTO 추가 #11

---
+++ src/main/java/com/OOL/oolfinance/dto/main/StockTableDTO.java
@@ -0,0 +1,33 @@
+package com.OOL.oolfinance.dto.main;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : StockTableDTO
+ * @date : 3/26/25 / 11:47 PM
+ * @modifyed : $
+ **/
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class StockTableDTO {
+    String stockSymbol;
+
+    String stockName;
+
+    BigDecimal previousClose;
+
+    BigDecimal currentClose;
+
+    BigDecimal tradingValue;
+
+    long stockVolume;
+}
