c17fc8f05dfc0e8e65abd45a1e79712675964bec
Pin <ppyj48625@naver.com>
2025-03-27 00:05:19 +0900

    feat: 지수 관련 Controller 추가 #11

---
+++ src/main/java/com/OOL/oolfinance/controller/index/IndexRestController.java
@@ -0,0 +1,24 @@
+package com.OOL.oolfinance.controller.index;
+
+import com.OOL.oolfinance.dto.index.IndexResponse;
+import com.OOL.oolfinance.enums.IndexStatus;
+import com.OOL.oolfinance.service.index.IndexService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : IndexRestController
+ * @date : 3/25/25 / 10:02 PM
+ * @modifyed : $
+ **/
+
+@RestController
+@RequiredArgsConstructor
+public class IndexRestController {
+}
