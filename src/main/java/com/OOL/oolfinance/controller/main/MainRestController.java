ee17b490b865fe81fe94f745b0d09bef0cc9acb0
Pin <ppyj48625@naver.com>
2025-03-31 23:55:54 +0900

    feat: 주식 리스트 조회용 GET 엔드포인트 추가 #11

--- src/main/java/com/OOL/oolfinance/controller/main/MainRestController.java
+++ src/main/java/com/OOL/oolfinance/controller/main/MainRestController.java
@@ -1,10 +1,15 @@
 package com.OOL.oolfinance.controller.main;
 
 import com.OOL.oolfinance.dto.main.IndexDTO;
+import com.OOL.oolfinance.dto.main.StockTableDTO;
+import com.OOL.oolfinance.enums.CountryStatus;
 import com.OOL.oolfinance.enums.IndexStatus;
+import com.OOL.oolfinance.enums.StockSortType;
 import com.OOL.oolfinance.service.index.IndexService;
+import com.OOL.oolfinance.service.stock.StockService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,11 +26,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MainRestController {
     private final IndexService indexService;
+    private final StockService stockService;
 
     @GetMapping(value = "/api/main/indices")
-    public List<IndexDTO> getIndicesList(IndexStatus requestStatus) {
+    public List<IndexDTO> getIndicesList(@RequestParam(value = "requestStatus") IndexStatus requestStatus) {
 
         return indexService.fetchIndexList(requestStatus);
     }
 
+    @GetMapping(value = "/api/main/stocks")
+    public List<StockTableDTO> getStockList(@RequestParam(defaultValue = "0") int page,
+                                            @RequestParam(value = "countryStatus") CountryStatus countryStatus,
+                                            @RequestParam(value = "sort") StockSortType sortType) {
+        return stockService.getStockTable(page, countryStatus, sortType);
+    }
 }
