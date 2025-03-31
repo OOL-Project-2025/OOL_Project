f05fbb25db99a710ecef55281e33d0071761decd
Pin <ppyj48625@naver.com>
2025-03-31 23:51:02 +0900

    feat: StockTableDTO 리스트를 반환하는 findByCountryStatus 추가 #11

--- src/main/java/com/OOL/oolfinance/repository/stock/StockCustomRepository.java
+++ src/main/java/com/OOL/oolfinance/repository/stock/StockCustomRepository.java
@@ -1,5 +1,12 @@
 package com.OOL.oolfinance.repository.stock;
 
+import com.OOL.oolfinance.dto.main.StockTableDTO;
+import com.OOL.oolfinance.enums.CountryStatus;
+import com.OOL.oolfinance.enums.StockSortType;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 /**
  * @author : yongjukim
  * @version : 1.0.0
@@ -9,4 +16,5 @@ package com.OOL.oolfinance.repository.stock;
  * @modifyed : $
  **/
 public interface StockCustomRepository {
+    List<StockTableDTO> findByCountryStatus(Pageable pageable, CountryStatus countryStatus, StockSortType sortType);
 }
--- src/main/java/com/OOL/oolfinance/repository/stock/StockRepository.java
+++ src/main/java/com/OOL/oolfinance/repository/stock/StockRepository.java
@@ -14,5 +14,5 @@ import org.springframework.stereotype.Repository;
  **/
 
 @Repository
-public interface StockRepository extends JpaRepository<Stock, String> {
+public interface StockRepository extends JpaRepository<Stock, String>, StockCustomRepository {
 }
