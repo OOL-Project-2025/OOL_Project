28ff60ca03c88805ac54c4b80aff3e0d9496356e
zyoung-herazy <wlwl10000@naver.com>
2025-04-03 17:30:45 +0900

    chore: setter 어노테이션 삭제

--- src/main/java/com/OOL/oolfinance/dto/MemberDTO.java
+++ src/main/java/com/OOL/oolfinance/dto/MemberDTO.java
@@ -3,11 +3,9 @@ package com.OOL.oolfinance.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
