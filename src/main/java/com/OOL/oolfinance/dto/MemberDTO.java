2a1bcf53e2cf53f021382beb8c914095daa363d7
zyoung-herazy <wlwl10000@naver.com>
2025-04-09 23:18:26 +0900

    feat: MemberDTO에 Entity 변환 메서드 추가 (toEntity, toMemberDTO)

--- src/main/java/com/OOL/oolfinance/dto/MemberDTO.java
+++ src/main/java/com/OOL/oolfinance/dto/MemberDTO.java
@@ -1,5 +1,7 @@
 package com.OOL.oolfinance.dto;
 
+import com.OOL.oolfinance.entity.member.Member;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,4 +15,21 @@ public class MemberDTO {
 	private String memberId;
 	private String memberPassword;
 	private String memberNickname;
+	
+	public static MemberDTO toMemberDTO(Member member) {
+		return new MemberDTO(
+            member.getMemberId(),
+            member.getPassword(),
+            member.getNickname()
+        );
+	}
+	
+	public Member toEntity() {
+	    return Member.builder()
+	            .memberId(this.memberId)
+	            .password(this.memberPassword)
+	            .nickname(this.memberNickname)
+	            .build();
+	}
+
 }
