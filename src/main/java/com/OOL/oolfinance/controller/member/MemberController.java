74e493e759ae589b0f0dd5152ea0e7d47f029507
zyoung-herazy <wlwl10000@naver.com>
2025-04-09 23:05:46 +0900

    refactor: 로그인 및 회원가입 처리 방식 개선 및 리다이렉트 경로 수정

--- src/main/java/com/OOL/oolfinance/controller/member/MemberController.java
+++ src/main/java/com/OOL/oolfinance/controller/member/MemberController.java
@@ -2,12 +2,14 @@ package com.OOL.oolfinance.controller.member;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.OOL.oolfinance.dto.MemberDTO;
 import com.OOL.oolfinance.service.member.MemberService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -20,14 +22,40 @@ public class MemberController {
 	//회원가입 페이지 출력 요청
 	@GetMapping("/member/save")
 	public String saveForm() {
-		return "save";
+		return "/save.html";
 	}
 	
-	@PostMapping("/member/save")
-	public String save(@ModelAttribute MemberDTO memberDTO) {
-		System.out.println("MemberController.save");
-		System.out.println("memberDTO = " + memberDTO);
-		memberService.signup(memberDTO);
-		return "login";
+	 @PostMapping("/member/save")
+	    public String save(HttpServletRequest request) {
+	        String id = request.getParameter("memberId");
+	        String pw = request.getParameter("memberPassword");
+	        String nickname = request.getParameter("memberNickname");
+
+	        MemberDTO memberDTO = new MemberDTO(id, pw, nickname);
+	        System.out.println("memberDTO = " + memberDTO);
+	        memberService.signup(memberDTO);
+	        return "redirect:/member/login";
+	    }
+	
+	@GetMapping("/member/login")
+	public String loginForm() {
+		return "redirect:/login.html";
+	}
+	
+	@PostMapping("/member/login")
+	public String login(@RequestParam("memberId") String memberId, @RequestParam("memberPassword") String memberPassword, HttpSession session ) {
+		
+		MemberDTO memberDTO = new MemberDTO(memberId, memberPassword, null);
+		MemberDTO loginResult = memberService.login(memberDTO);
+		if(loginResult != null) {
+			//login 성공
+			session.setAttribute("loginId", loginResult.getMemberId());
+			 System.out.println("로그인 성공! loginId = " + loginResult.getMemberId());
+			return "redirect:/main.html";
+		} else {
+			// login 실패
+			System.out.println("로그인 실패");
+			return "redirect:/member/login?error=true";
+		}
 	}
 }
