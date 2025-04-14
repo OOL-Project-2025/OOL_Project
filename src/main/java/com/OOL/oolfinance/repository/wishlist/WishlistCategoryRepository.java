9b68a06fcf48740cb3b676c4d84593c929350db6
zyoung-herazy <wlwl10000@naver.com>
2025-04-14 22:05:18 +0900

    feat: findByMember 메서드 추가

--- src/main/java/com/OOL/oolfinance/repository/wishlist/WishlistCategoryRepository.java
+++ src/main/java/com/OOL/oolfinance/repository/wishlist/WishlistCategoryRepository.java
@@ -1,8 +1,12 @@
 package com.OOL.oolfinance.repository.wishlist;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.OOL.oolfinance.entity.member.Member;
 import com.OOL.oolfinance.entity.wishlist.Wishlist;
 
 public interface WishlistCategoryRepository extends JpaRepository<Wishlist, Long> {
-
+	List<Wishlist> findByMember(Member member);
 }
