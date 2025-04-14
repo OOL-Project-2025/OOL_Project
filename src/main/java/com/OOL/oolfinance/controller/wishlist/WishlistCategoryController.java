03e6c57a5d59783767ea1adacaf8577d76f0f06c
zyoung-herazy <wlwl10000@naver.com>
2025-04-14 22:04:13 +0900

    feat: 위시리스트 카테고리 추가 기능 구현

--- src/main/java/com/OOL/oolfinance/controller/wishlist/WishlistCategoryController.java
+++ src/main/java/com/OOL/oolfinance/controller/wishlist/WishlistCategoryController.java
@@ -3,6 +3,7 @@ package com.OOL.oolfinance.controller.wishlist;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -10,7 +11,7 @@ import com.OOL.oolfinance.dto.WishlistDTO;
 import com.OOL.oolfinance.entity.member.Member;
 import com.OOL.oolfinance.service.wishlist.WishlistCategoryService;
 
-import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -29,10 +30,15 @@ public class WishlistCategoryController {
 	}
 	
 	@PostMapping("/wishlist/category/do")
-	public String wishlistAdd(Model model, CategoryInput parameter) {
-		
-		boolean result = wishlistCategoryService.categoryAdd(parameter.getCategoryName());
-		 
-		return "redirect:/wishlistCategory.html";
+	public String wishlistAdd(HttpSession session, CategoryInput parameter) {
+		String memberId = (String) session.getAttribute("loginId"); // 로그인한 사용자 아이디 가져오기
+		if (memberId == null) {
+			return "redirect:/member/login"; // 로그인 안되어 있으면 로그인 페이지로
+		}
+
+		wishlistCategoryService.categoryAdd(memberId,parameter.getCategoryName());
+		return "wishlistCategory.html";
 	}
+	
+	
 }
