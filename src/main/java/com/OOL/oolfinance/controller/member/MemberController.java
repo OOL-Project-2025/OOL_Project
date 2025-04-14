52b308194ffdddec710bd90a1914c3e59bd3cf96
zyoung-herazy <wlwl10000@naver.com>
2025-04-14 23:07:55 +0900

    feat: 회원 탈퇴를 위한 DeleteMapping 추가

--- src/main/java/com/OOL/oolfinance/controller/member/MemberController.java
+++ src/main/java/com/OOL/oolfinance/controller/member/MemberController.java
@@ -2,7 +2,9 @@ package com.OOL.oolfinance.controller.member;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -75,6 +77,12 @@ public class MemberController {
 	    memberDTO.setMemberId(loginId);
 	    memberService.memberUpdate(memberDTO);
 	    return ResponseEntity.ok("업데이트 성공");
+	}
+	
+	@DeleteMapping("/member/delete/{memberId}")
+	public ResponseEntity<?> deleteMember(@PathVariable("memberId") String memberId) {
+	    memberService.deleteMember(memberId);
+	    return ResponseEntity.ok().build();
 	}
 }
 
