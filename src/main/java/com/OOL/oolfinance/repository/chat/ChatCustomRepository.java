e3464a6ceda7601932d2605dcc8056514f102450
Pin <ppyj48625@naver.com>
2025-05-13 00:15:52 +0900

    feat: chat 관련 repository 추가 #17

---
+++ src/main/java/com/OOL/oolfinance/repository/chat/ChatCustomRepository.java
@@ -0,0 +1,12 @@
+package com.OOL.oolfinance.repository.chat;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : ChatCustomRepository
+ * @date : 2025. 5. 8. / 오후 8:11
+ * @modifyed :
+ **/
+public interface ChatCustomRepository {
+}
---
+++ src/main/java/com/OOL/oolfinance/repository/chat/ChatCustomRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.OOL.oolfinance.repository.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : ChatCustomRepositoryImpl
+ * @date : 2025. 5. 8. / 오후 8:11
+ * @modifyed :
+ **/
+
+@Repository
+@RequiredArgsConstructor
+public class ChatCustomRepositoryImpl implements ChatCustomRepository {
+}
