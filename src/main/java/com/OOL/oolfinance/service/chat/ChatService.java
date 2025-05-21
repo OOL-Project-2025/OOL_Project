6a2fab645a36c7923929c0b0a93afa93e6041430
Pin <ppyj48625@naver.com>
2025-05-22 00:44:35 +0900

    feat: chat 이전 기록 불러오기 Service 메소드 추가 #17

--- src/main/java/com/OOL/oolfinance/service/chat/ChatService.java
+++ src/main/java/com/OOL/oolfinance/service/chat/ChatService.java
@@ -1,6 +1,9 @@
 package com.OOL.oolfinance.service.chat;
 
 import com.OOL.oolfinance.dto.chat.ChatDTO;
+import com.OOL.oolfinance.dto.chat.ChatHistoryDTO;
+
+import java.util.List;
 
 /**
  * @author : yongjukim
@@ -12,4 +15,6 @@ import com.OOL.oolfinance.dto.chat.ChatDTO;
  **/
 public interface ChatService {
     void chatSave(ChatDTO chatDTO);
+
+    List<ChatHistoryDTO> getPreviousChat(long chatId);
 }
--- src/main/java/com/OOL/oolfinance/service/chat/ChatServiceImpl.java
+++ src/main/java/com/OOL/oolfinance/service/chat/ChatServiceImpl.java
@@ -1,10 +1,18 @@
 package com.OOL.oolfinance.service.chat;
 
 import com.OOL.oolfinance.dto.chat.ChatDTO;
+import com.OOL.oolfinance.dto.chat.ChatHistoryDTO;
 import com.OOL.oolfinance.entity.chat.Chat;
 import com.OOL.oolfinance.repository.chat.ChatRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author : yongjukim
@@ -31,4 +39,15 @@ public class ChatServiceImpl implements ChatService {
 
         chatRepository.save(chat);
     }
+
+    @Override
+    public List<ChatHistoryDTO> getPreviousChat(long chatId) {
+        Pageable pageable = PageRequest.of(0, 20);
+
+        List<ChatHistoryDTO> chatHistoryDTOList = chatRepository.findByChatIdLessThanOrderByChatIdDesc(chatId, pageable);
+        Collections.reverse(chatHistoryDTOList);
+
+        return chatHistoryDTOList;
+    }
+
 }
