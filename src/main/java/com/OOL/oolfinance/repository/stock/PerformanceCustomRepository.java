48aa97ea11ba6a4ee9e49fbf3bca26a284fbc626
Pin <ppyj48625@naver.com>
2025-07-09 23:54:04 +0900

    feat: 실적 관련 repository 추가 #24

---
+++ src/main/java/com/OOL/oolfinance/repository/stock/PerformanceCustomRepository.java
@@ -0,0 +1,12 @@
+package com.OOL.oolfinance.repository.stock;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : PerformanceCustomRepository
+ * @date : 2025. 7. 3. / 오후 12:34
+ * @modifyed :
+ **/
+public interface PerformanceCustomRepository {
+}
