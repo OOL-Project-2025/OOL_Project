5e7fccd9e4252cc737b3b43c2f7ba0d781ba65b2
Pin <ppyj48625@naver.com>
2025-07-09 23:34:36 +0900

    feat: 최신 차트 봉 검색 메서드 추가 #24

--- src/main/java/com/OOL/oolfinance/repository/chart/ChartRepository.java
+++ src/main/java/com/OOL/oolfinance/repository/chart/ChartRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author : yongjukim
@@ -18,5 +19,9 @@ import java.time.LocalDateTime;
 @Repository
 public interface ChartRepository extends JpaRepository<Chart, Long>, ChartCustomRepository {
 
+    Chart findTopByStockCodeOrderByChartDateTimeDesc(String stockCode);
+
     boolean existsByStockCodeAndChartDateTime(String stockCode, LocalDateTime chartDateTime);
+
+    List<Chart> findTop2ByStockCodeOrderByChartDateTimeDesc(String stockCode);
 }
