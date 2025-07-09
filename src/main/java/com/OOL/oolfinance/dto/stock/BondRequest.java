e75fb7e21eeda955894882cc63601fee1813dcfc
Pin <ppyj48625@naver.com>
2025-07-10 00:01:29 +0900

    feat: 데이터 맵핑을 위한 Request, Wrapper 추가 #24

---
+++ src/main/java/com/OOL/oolfinance/dto/stock/BondRequest.java
@@ -0,0 +1,31 @@
+package com.OOL.oolfinance.dto.stock;
+
+import lombok.Data;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : BondRequest
+ * @date : 2025. 7. 8. / 오후 11:16
+ * @modifyed :
+ **/
+
+@Data
+public class BondRequest {
+    private String localTradedAt; // 날짜
+    private String closePrice; // 종가
+    private FluctuationsType fluctuationsType; // 상승/하락상태
+    private String fluctuations; // 전일 대비 종가
+    private String fluctuationsRatio; // 등락률
+    private String openPrice; // 시가
+    private String highPrice; // 고가
+    private String lowPrice; // 저가
+
+    @Data
+    public static class FluctuationsType {
+        private String code;
+        private String text; // 상승/하락
+        private String name;
+    }
+}
