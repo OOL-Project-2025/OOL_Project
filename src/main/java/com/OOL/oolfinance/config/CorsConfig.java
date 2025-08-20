5305840b9808be8038fd8dc4534c32429f1ee6e6
Pin <ppyj48625@naver.com>
2025-08-20 23:35:30 +0900

    feat: oauth2 로그인을 위한 SecurityConfig 내용 추가 #21

---
+++ src/main/java/com/OOL/oolfinance/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.OOL.oolfinance.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : yongjukim
+ * @version : 1.0.0
+ * @package : oolfinance
+ * @name : CorsConfig
+ * @date : 2025. 8. 13. / 오후 4:08
+ * @modifyed :
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://client");
+    }
+}
