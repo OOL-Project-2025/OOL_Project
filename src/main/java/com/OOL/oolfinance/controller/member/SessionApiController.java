1c6fd4e182b8aacb94f32970459471d9d7dd54da
Pin <ppyj48625@naver.com>
2025-08-20 23:38:32 +0900

    chore: 세션을 사용하지 않음에 따라 확인용 코드 임시 작성 #21

--- src/main/java/com/OOL/oolfinance/controller/member/SessionApiController.java
+++ src/main/java/com/OOL/oolfinance/controller/member/SessionApiController.java
@@ -3,30 +3,55 @@ package com.OOL.oolfinance.controller.member;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.OOL.oolfinance.dto.MemberDTO;
 import com.OOL.oolfinance.dto.general.GeneralResponse;
+import com.OOL.oolfinance.entity.member.Member;
 import com.OOL.oolfinance.enums.StatusEnum;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SessionApiController {
 
 	@GetMapping("/api/session/login-id")
-    public ResponseEntity<GeneralResponse<String>> getLoginId(HttpSession session) {
-        Object loginId = session.getAttribute("loginId");
+    public ResponseEntity<GeneralResponse<MemberDTO>> getLoginId(@AuthenticationPrincipal Member member) {
+        String loginId = member.getProviderId();
+        String nickname = member.getNickname();
+        String password = member.getPassword();
+        String platform = member.getProvider();
+
+        log.info(loginId);
+        log.info(nickname);
+        log.info(password);
+        log.info(platform);
+
+        MemberDTO memberDTO = MemberDTO.builder()
+                .providerId("NO_SESSION")
+                .build();
+
         if (loginId != null) {
-            return ResponseEntity.ok(GeneralResponse.<String>builder()
+            memberDTO = MemberDTO.builder()
+                    .memberNickname(nickname)
+                    .memberPassword(password)
+                    .provider(platform)
+                    .providerId(loginId)
+                    .build();
+
+            return ResponseEntity.ok(GeneralResponse.<MemberDTO>builder()
                             .status(StatusEnum.OK)
                             .message("success")
-                            .data(loginId.toString())
+                            .data(memberDTO)
                     .build());
         } else {
-            return ResponseEntity.ok(GeneralResponse.<String>builder()
-                    .status(StatusEnum.OK)
-                    .message("success")
-                    .data("NO_SESSION")
+            return ResponseEntity.ok(GeneralResponse.<MemberDTO>builder()
+                    .status(StatusEnum.UNAUTHORIZED)
+                    .message("null")
+                    .data(memberDTO)
                     .build());
         }
     }
