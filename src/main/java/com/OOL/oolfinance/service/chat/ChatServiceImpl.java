d579efd77fe339ee2f9f7c10e2760963a052b56e
Pin <ppyj48625@naver.com>
2025-09-12 00:41:19 +0900

    fix: @AuthenticationPrincipal을 사용하려 했으나 해결하지 못한 문제로 인해 Principal을 사용 및 그에 따른 수정 #21

--- src/main/java/com/OOL/oolfinance/service/chat/ChatServiceImpl.java
+++ src/main/java/com/OOL/oolfinance/service/chat/ChatServiceImpl.java
@@ -3,12 +3,16 @@ package com.OOL.oolfinance.service.chat;
 import com.OOL.oolfinance.dto.chat.ChatDTO;
 import com.OOL.oolfinance.dto.chat.ChatHistoryDTO;
 import com.OOL.oolfinance.entity.chat.Chat;
+import com.OOL.oolfinance.entity.member.CustomUserDetails;
 import com.OOL.oolfinance.repository.chat.ChatRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -23,22 +27,31 @@ import java.util.List;
  * @modifyed :
  **/
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatServiceImpl implements ChatService {
     private final ChatRepository chatRepository;
 
     @Override
-    public void chatSave(ChatDTO chatDTO) {
+    public Chat chatSave(ChatDTO chatDTO, Principal principal) {
+        UsernamePasswordAuthenticationToken auth =
+                (UsernamePasswordAuthenticationToken) principal;
+        CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
+
+        log.info("hihihi");
+        log.info(chatDTO.getSendTime().toString());
         Chat chat = Chat.builder()
-                .provider(chatDTO.getProvider())
-                .providerId(chatDTO.getProviderId())
-                .nickname(chatDTO.getNickname())
+                .provider(user.getProvider())
+                .providerId(user.getProviderId())
+                .nickname(user.getUsername())
                 .message(chatDTO.getMessage())
                 .time(chatDTO.getSendTime())
                 .build();
-
+        log.info(chat.getTime().toString());
         chatRepository.save(chat);
+
+        return chat;
     }
 
     @Override
