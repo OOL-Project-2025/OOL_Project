96a157a5dfb7e1262eaed8c3b085b612f5e30bda
Pin <ppyj48625@naver.com>
2025-09-14 20:36:23 +0900

    fix: cors 설정 변경 #21

--- src/main/java/com/OOL/oolfinance/config/StompWebSocketConfig.java
+++ src/main/java/com/OOL/oolfinance/config/StompWebSocketConfig.java
@@ -35,11 +35,11 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .addInterceptors(jwtHandshakeInterceptor)
-                .setAllowedOrigins("*");
+                .setAllowedOriginPatterns("http://localhost", "http://oolfinance.kro.kr");
 
         registry.addEndpoint("/ws")
                 .addInterceptors(jwtHandshakeInterceptor)
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns("http://localhost", "http://oolfinance.kro.kr")
                 .withSockJS();
     }
 
