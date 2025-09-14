21887ac59b0f0accd867fee206b13f1e94e0d2b6
Pin <ppyj48625@naver.com>
2025-09-14 21:14:29 +0900

    chore: localhost 라이브 서버 테스트용 cors 허용 #21

--- src/main/java/com/OOL/oolfinance/config/CorsConfig.java
+++ src/main/java/com/OOL/oolfinance/config/CorsConfig.java
@@ -19,6 +19,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOriginPatterns("http://localhost", "http://oolfinance.kro.kr");
+                .allowedOriginPatterns("http://localhost:*", "http://oolfinance.kro.kr");
     }
 }
